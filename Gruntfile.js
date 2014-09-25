131221b2e284890656cf4997e25ee86bcc61b27e
Jonathan Man <jon.man@rockaboxmedia.com>
2014-09-25 17:50:23 +0100

    E2E test that actually works

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,31 +4,47 @@ module.exports = function (grunt) {
 
     require('load-grunt-tasks')(grunt);
 
+    var path = 'node_modules/karma-abe-json2js-preprocessor/';
+
     grunt.initConfig({
+        copy: {
+            all: {
+                files: [
+                    {expand: true, src: 'lib/*', dest: path},
+                    {expand: true, src: 'package.json', dest: path}
+                ]
+            }
+        },
         jscs: {
             options: {
                 config: '.jscsrc'
             },
-            src: [
-                '**/*.js',
-                '!node_modules/**'
-            ]
+            src: ['**/*.js', '!node_modules/**']
         },
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
             },
-            src: [
-                '**/*.js',
-                '!node_modules/**'
-            ]
+            src: ['**/*.js', '!node_modules/**']
         },
         karma: {
             e2e: {
                 configFile: 'test/e2e/karma.conf.js'
             }
+        },
+        mkdir: {
+            all: {
+                options: {
+                    create: [path]
+                }
+            }
         }
     });
+
+    grunt.registerTask('setup', [
+        'mkdir',
+        'copy'
+    ]);
 
     grunt.registerTask('lint', [
         'jshint:src',
@@ -37,6 +53,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', [
         'lint',
+        'setup',
         'karma:e2e'
     ]);
 };
